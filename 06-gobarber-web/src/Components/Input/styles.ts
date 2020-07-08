b93a06b7f4f52e122cc124384168c8f596592e54
Eduardo Barros <joseeduardobarros@gmail.com>
2020-07-08 13:41:23 -0300

    Create Input component

---
+++ 06-gobarber-web/src/Components/Input/styles.ts
@@ -0,0 +1,80 @@
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isErrored: boolean;
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  border: solid 2px #232129;
+  color: #f4ede8;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  transition: border-color 0.2s;
+
+  & + & {
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+      color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  input {
+    color: #f4ede8;
+    background: transparent;
+    flex: 1;
+    border: 0;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    color: inherit;
+    margin-right: 16px;
+    transition: color 0.2s;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #ffffff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
